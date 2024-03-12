f38b81b72812a251fd3cb1c77db5ace908ac1596
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2024-03-12 13:18:07 +0530

    correction of chat bubble

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,17 +4,16 @@ const MessageContainer = () => {
   return (
     <div className="md:min-w-[450px] flex flex-col">
         <>
-        {/* Header */}
+            {/* Header */}
             <div className="bg-slate-500 px-4 py-2 mb-2">
-                <span className="label-text">To:</span>{" "}
-                <span className="text-gray-900 font-bold">John doe</span>
+                <span className="label-text">To:</span> <span className="text-gray-900 font-bold">John doe</span>
             </div> 
 
             <Messages />
             {/* <MessageInput /> */}
         </>
     </div>
-  )
-}
+  );
+};
 
-export default MessageContainer
+export default MessageContainer;
